9e538dddf79713740557b7163445d7e7d7a17cbb
MezanRH <Meaznurr@gmail.com>
2024-02-25 19:13:26 +0600

    Qwery website using Tailwind css

---
+++ js/script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        backgroundImage: {
+            'banner': "url('./images/banner.png')",
+        },
+        fontFamily: {
+          'open': ['Open Sans',],
+        },
+        
+      }
+    }
+  }
